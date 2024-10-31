46a546187b66e014675c8a1dfd0ac8547fb60d0f
cuongh1799 <cuongghuynhh1009@gmail.com>
2024-10-31 16:21:00 +0700

    Initial commit

---
+++ src/main/java/com/example/WouldYouPreferAnimeJava/WouldYouPreferAnimeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.WouldYouPreferAnimeJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WouldYouPreferAnimeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WouldYouPreferAnimeJavaApplication.class, args);
+	}
+
+}
